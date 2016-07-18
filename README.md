3c33baaa7533446add6c6770a75ed19821503766
Diego Muracciole <diegomuracciole@gmail.com>
2016-07-18 02:06:50 -0300

    Bootstrap commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<big><h1 align="center">react-style-log</h1></big>
---
+++ src/StyleLog.jsx
@@ -0,0 +1,9 @@
+import React, { PropTypes } from 'react';
+
+class StyleLog extends React.Component {
+  render() {
+    return false;
+  }
+}
+
+export default StyleLog;
