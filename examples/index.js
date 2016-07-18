7ca5cbfc8d1f776d11ada9124d96665c98d05466
Diego Muracciole <diegomuracciole@gmail.com>
2016-07-18 13:50:45 -0300

    Add examples project

---
+++ examples/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Log from 'react-log';
+
+ReactDOM.render(
+  <Log>
+    <h1 color="red">Title <span>colorful</span> is nice</h1>
+  </Log>,
+  document.getElementById('root')
+)
---
+++ examples/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'react-log$': path.join(__dirname, '..', 'src'),
+    },
+  }
+}
