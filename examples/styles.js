20fcd6ef7a9d615eca3845bf56ef4b850508bb93
Diego Muracciole <diegomuracciole@gmail.com>
2017-01-21 14:34:29 -0300

    Refactor example styles file

--- examples/styles.js
+++ examples/styles.js
@@ -1,27 +1,27 @@
 export default {
   lightText: {
-    fontFamily:"Open Sans, sans-serif",
-    color:"black",
-    padding:"0px 0px",
-    margin:"5px 0px 0px 0px",
-    fontSize:"35px"
+    fontFamily: 'Open Sans, sans-serif',
+    color: 'black',
+    padding: '0px 0px',
+    margin: '5px 0px 0px 0px',
+    fontSize: '35px',
   },
   boldText: {
-    fontFamily:"Open Sans, sans-serif",
-    color:"black",
+    fontFamily: 'Open Sans, sans-serif',
+    color: 'black',
     fontWeight: 'bold',
-    padding:"0px 0px",
-    margin:"5px 0px 0px 0px",
-    fontSize:"35px"
+    padding: '0px 0px',
+    margin: '5px 0px 0px 0px',
+    fontSize: '35px',
   },
   tag: {
-    fontFamily:"Arial, Helvetica, sans-serif",
+    fontFamily: 'Arial, Helvetica, sans-serif',
     color: 'black',
     padding: '5px 5px',
     margin: '0px 0px 0px 10px',
-    background:'#ffd10b',
-    fontSize:"25px",
+    background: '#ffd10b',
+    fontSize: '25px',
     fontWeight: 'bold',
-    borderRadius: '5px'
-  }
-}
+    borderRadius: '5px',
+  },
+};
