46ee24cb6f19633da47bbd5ba8b156c545709944
Diego Muracciole <diegomuracciole@gmail.com>
2017-01-22 02:35:33 -0300

    First page draft

--- src/compiler/index.js
+++
@@ -1,9 +0,0 @@
-import transformer from './transformer';
-import generator from './generator';
-
-export default (ast) => {
-  const newAst = transformer(ast);
-  const output = generator(newAst);
-
-  return output;
-};
