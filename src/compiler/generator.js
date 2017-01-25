b3810b94ff5e75a9c2c7c4bf7315ab21ba789543
Diego Muracciole <diegomuracciole@gmail.com>
2017-01-25 00:54:43 -0300

    Div and Span do not produce %c for styling

--- src/compiler/generator.js
+++ src/compiler/generator.js
@@ -37,13 +37,9 @@ const generator = (node) => {
     case 'body':
     case 'ul':
     case 'ol':
-      return renderedChildren;
     case 'span':
-      return {
-        value: '%c' + renderedChildren.value,
-        styles: renderedChildren.styles
-      };
     case 'div':
+      return renderedChildren;
     case 'h1':
     case 'h2':
     case 'h3':
